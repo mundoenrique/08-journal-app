08228f7d53f89567b5d6afca412b9d48de20dba3
mundoenrique <mundoenrique@hotmail.com>
2022-03-27 16:32:16 -0400

    Configura redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Provider } from 'react-redux';
+
 import AppRouter from './routers/AppRouter';
+import { store } from './store/store';
 
 function App() {
 	return (
-		<>
+		<Provider store={store}>
 			<AppRouter />
-		</>
+		</Provider>
 	);
 }
 
