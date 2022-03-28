d2e9d3429d65df777f06f46b03e01b79b5809a60
mundoenrique <mundoenrique@hotmail.com>
2022-03-28 12:27:08 -0400

    obtiene datos del formulario de registro

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
 
 export default function RegisterScreen() {
+	const [{ name, email, password, password2 }, handleInputValues] = useForm({
+		name: 'Yajaira',
+		email: 'yayita@gmail.com',
+		password: '123456',
+		password2: '123456',
+	});
+
+	const handleRegister = (e) => {
+		e.preventDefault();
+		console.log(name, email, password, password2);
+	};
+
 	return (
 		<>
 			<h3 className="auth__title">Register</h3>
-			<form>
+			<form onSubmit={handleRegister}>
 				<input
 					type="text"
 					placeholder="Name"
 					name="name"
 					className="auth__input"
 					autoComplete="off"
+					value={name}
+					onChange={handleInputValues}
 				/>
 				<input
 					type="text"
@@ -18,18 +33,24 @@ export default function RegisterScreen() {
 					name="email"
 					className="auth__input"
 					autoComplete="off"
+					value={email}
+					onChange={handleInputValues}
 				/>
 				<input
 					type="password"
 					placeholder="Password"
 					name="password"
 					className="auth__input"
+					value={password}
+					onChange={handleInputValues}
 				/>
 				<input
 					type="password"
 					placeholder="Password Confirmation"
 					name="password2"
 					className="auth__input"
+					value={password2}
+					onChange={handleInputValues}
 				/>
 				<button type="submit" className="btn btn-primary btn-block mb-5">
 					Register
